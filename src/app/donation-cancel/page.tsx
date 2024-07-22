4c45370fb67fe8f0f00fde16ed77bc2807eb9f57
Kani <gkounkoud@myges.fr>
2024-07-22 02:20:25 +0200

    chore: Update import statement for next/router in donation-cancel page

--- src/app/donation-cancel/page.tsx
+++ src/app/donation-cancel/page.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
 export default function DonationCancel() {
